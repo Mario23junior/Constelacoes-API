d8b773d49ae016a95fcb9b995b5452ec0f28294b
Mario23junior <mariojunior3251@gmail.com>
2021-07-01 00:52:50 -0300

    Listando informacoes de catalogacao por id

--- src/main/java/com/project/constelacoes/Controller/Service/ServiceCatalogacao.java
+++ src/main/java/com/project/constelacoes/Controller/Service/ServiceCatalogacao.java
@@ -1,5 +1,7 @@
 package com.project.constelacoes.Controller.Service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.project.constelacoes.Model.Catalogacao;
@@ -8,14 +10,19 @@ import com.project.constelacoes.Repository.CatalogacaoRepository;
 @Service
 public class ServiceCatalogacao {
     
-	private CatalogacaoRepository catoloCatalogacaoRepository;
+	private CatalogacaoRepository catalogacaoRepository;
     
-	public ServiceCatalogacao(CatalogacaoRepository catoloCatalogacaoRepository) {
-		this.catoloCatalogacaoRepository = catoloCatalogacaoRepository;
+	public ServiceCatalogacao(CatalogacaoRepository catalogacaoRepository) {
+		this.catalogacaoRepository = catalogacaoRepository;
  	}
 	
 	public Catalogacao salvar(Catalogacao catalogacao){
-		return catoloCatalogacaoRepository.save(catalogacao);
+		return catalogacaoRepository.save(catalogacao);
+	}
+	
+	public Optional<Catalogacao> listDataById(Long id) {
+		Optional<Catalogacao> list = catalogacaoRepository.findById(id);
+		return list;
 	}
 	
 }
