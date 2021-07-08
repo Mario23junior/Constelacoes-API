648bc17edc84717f67b63d5ef257e176882bdabf
Mario23junior <mariojunior3251@gmail.com>
2021-07-07 23:48:38 -0300

    Criando camada de service e controller de coordenadas

--- src/main/java/com/project/constelacoes/Model/Catalogacao.java
+++ src/main/java/com/project/constelacoes/Model/Catalogacao.java
@@ -4,25 +4,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
 
- 
- 
+
 @Entity
 public class Catalogacao {
     
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
-	@NotBlank(message = "{campo.catalogacao.visibilidadeOlhoNu}")
-	private Boolean visibilidadeOlhoNu;
-	
-	@NotBlank(message = "{campo.catalogacao.estrelaPrincipal}")
-	private String estrelaPrincipal;	
-	
-	@NotBlank(message = "{campo.catalogacao.magnitudeAparente}")
- 	private String magnitudeAparente;
+ 	private Boolean visibilidadeOlhoNu;
+ 	private String estrelaPrincipal;		
+  	private String magnitudeAparente;
 	
 	public Long getId() {
 		return id;
