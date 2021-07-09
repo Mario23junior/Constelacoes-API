8a806e096024c46a732c69e3258d4751cf3889ee
Mario23junior <mariojunior3251@gmail.com>
2021-07-09 18:47:02 -0300

    Salvando dados observacionais

--- src/main/java/com/project/constelacoes/Controller/Service/ServiceDadosObservacionais.java
+++ src/main/java/com/project/constelacoes/Controller/Service/ServiceDadosObservacionais.java
@@ -2,7 +2,19 @@ package com.project.constelacoes.Controller.Service;
 
 import org.springframework.stereotype.Service;
 
+import com.project.constelacoes.Model.DadosObservacionais;
+import com.project.constelacoes.Repository.DadosObservacionaisRepository;
+
 @Service
 public class ServiceDadosObservacionais {
-
+   
+	private DadosObservacionaisRepository dadosObservacionaisRepository;
+	
+	public ServiceDadosObservacionais(DadosObservacionaisRepository dadosObservacionaisRepository) {
+		 this.dadosObservacionaisRepository = dadosObservacionaisRepository;
+	}
+	
+	public DadosObservacionais saveDadosObs(DadosObservacionais dadosObservacionais) {
+		return dadosObservacionaisRepository.save(dadosObservacionais);
+ 	}
 }
