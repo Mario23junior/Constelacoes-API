ae128e8d291f9606ae18c65f0f1e61f74535f049
Mario23junior <mariojunior3251@gmail.com>
2021-07-10 12:21:46 -0300

    Listando informacoes observacionais da base de dados

--- src/main/java/com/project/constelacoes/Controller/Service/ServiceDadosObservacionais.java
+++ src/main/java/com/project/constelacoes/Controller/Service/ServiceDadosObservacionais.java
@@ -1,7 +1,10 @@
 package com.project.constelacoes.Controller.Service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.project.constelacoes.Exceptions.NotValueDataCatalogacao;
 import com.project.constelacoes.Model.DadosObservacionais;
 import com.project.constelacoes.Repository.DadosObservacionaisRepository;
 
@@ -17,4 +20,10 @@ public class ServiceDadosObservacionais {
 	public DadosObservacionais saveDadosObs(DadosObservacionais dadosObservacionais) {
 		return dadosObservacionaisRepository.save(dadosObservacionais);
  	}
+	
+	public Optional<DadosObservacionais> listIdData(Long id){
+		Optional<DadosObservacionais> dadosObser = dadosObservacionaisRepository.findById(id);
+		dadosObser.orElseThrow(() -> new NotValueDataCatalogacao("Valos de observação não encontrado"));
+		return dadosObser;
+	}
 }
