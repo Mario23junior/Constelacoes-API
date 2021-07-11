969cf2a986b1db5f3200b318f9267e6b88b6297a
Mario23junior <mariojunior3251@gmail.com>
2021-07-11 12:37:13 -0300

    Listando dados por id ta bela Fatos Costelacaoes

--- src/main/java/com/project/constelacoes/Controller/Service/ServiceFatosCostelacao.java
+++ src/main/java/com/project/constelacoes/Controller/Service/ServiceFatosCostelacao.java
@@ -1,7 +1,11 @@
 package com.project.constelacoes.Controller.Service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.project.constelacoes.Exceptions.NotValueDataCatalogacao;
+import com.project.constelacoes.Model.FatosCostelacao;
 import com.project.constelacoes.Repository.FatosCostelacaoRepository;
 
 @Service
@@ -13,5 +17,13 @@ public class ServiceFatosCostelacao {
 		this.fatosCostelacaoRepository = fatosCostelacaoRepository;
  	}
 	
+	public FatosCostelacao save(FatosCostelacao fatosCostelacao) {
+		return fatosCostelacaoRepository.save(fatosCostelacao);
+	}
 	
-}
+	public Optional<FatosCostelacao> list(Long id){
+	  Optional<FatosCostelacao> informaDados = fatosCostelacaoRepository.findById(id);
+      informaDados.orElseThrow(() -> new NotValueDataCatalogacao("informação não encontrada"));
+		return informaDados;
+ 	}
+}  
