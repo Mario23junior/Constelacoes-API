32e33b304b25f218e424ba66528d2e110491bf8b
Mario23junior <mariojunior3251@gmail.com>
2021-07-11 13:24:05 -0300

    Atualizando informacoes da tabela fatosCostelacao

--- src/main/java/com/project/constelacoes/Controller/ControllerFatosCostelacao.java
+++ src/main/java/com/project/constelacoes/Controller/ControllerFatosCostelacao.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,7 @@ public class ControllerFatosCostelacao {
    }
    
    @PostMapping
-   public FatosCostelacao save(@RequestBody FatosCostelacao fatosCostelacao) {
+   public FatosCostelacao saveInfo(@RequestBody FatosCostelacao fatosCostelacao) {
 	   return serviceFatosCostelacao.save(fatosCostelacao);
    }
    
@@ -31,4 +32,10 @@ public class ControllerFatosCostelacao {
    public Optional<FatosCostelacao> listId(@PathVariable Long id){
 	   return serviceFatosCostelacao.list(id);
    }
+   
+   @PutMapping("/{id}")
+   public void updateFatos(@PathVariable Long id, @RequestBody FatosCostelacao fatosCostelacao){
+	    serviceFatosCostelacao.update(id, fatosCostelacao);
+   }
+   
 }
