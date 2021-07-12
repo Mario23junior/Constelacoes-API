f4ba68d3a631880e448a08e2eee9f7d016cc17e5
Mario23junior <mariojunior3251@gmail.com>
2021-07-12 00:16:43 -0300

    Criando controllerDTO e listando dados DTO finais

--- src/main/java/com/project/constelacoes/Service/Implements.java
+++ src/main/java/com/project/constelacoes/Service/Implements.java
@@ -1,6 +1,17 @@
 package com.project.constelacoes.Service;
 
+import java.util.List;
+
+import com.project.constelacoes.DTOModel.CatalogacaoDTO;
+import com.project.constelacoes.Model.Catalogacao;
+
 public interface Implements {
-   
+    
+	public List<CatalogacaoDTO> ListAllInformations();
+	public CatalogacaoDTO ConvertToCatalogocaoToCatalogacaoDTO(Catalogacao catalogacao);
+	public List<CatalogacaoDTO> listAllDataDTO();
+
+
+
 
 }
